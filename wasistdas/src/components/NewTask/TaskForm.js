95330d6fa64e462c49b2d0d8f06a9defd9d6b167
Ondřej Zaplatílek <ondrej.zaplatilek@tuta.io>
2022-01-09 12:58:36 +0100

    foodApp - default

---
+++ wasistdas/src/components/NewTask/TaskForm.js
@@ -0,0 +1,33 @@
+import { useImperativeHandle, useRef } from 'react';
+import { useEffect } from 'react/cjs/react.development';
+
+import classes from './TaskForm.module.css';
+
+const TaskForm = (props) => {
+  const taskInputRef = useRef();
+  const btn = useRef()
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredValue = taskInputRef.current.value;
+
+    if (enteredValue.trim().length > 0) {
+      props.onEnterTask(enteredValue);
+    }
+  };
+
+  useEffect(()=> {
+    taskInputRef.current.focus()
+    console.log("Task Input");
+    taskInputRef.current.value = ""
+  })
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <input type='text' ref={taskInputRef} />
+      <button ref={btn}>{props.loading ? 'Sending...' : 'Add'}</button>
+    </form>
+  );
+};
+
+export default TaskForm;
